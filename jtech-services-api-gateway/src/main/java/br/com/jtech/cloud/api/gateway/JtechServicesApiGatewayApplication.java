9a53bcdfdc8b88f4dcee7ef3b11811ac39a20660
Angelo Vicente Filho <angelo.vicente@jtech.com.br>
2021-07-05 14:15:58 -0300

    [main] Create Api Gateway Service

---
+++ jtech-services-api-gateway/src/main/java/br/com/jtech/cloud/api/gateway/JtechServicesApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.jtech.cloud.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtechServicesApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtechServicesApiGatewayApplication.class, args);
+	}
+
+}
---
+++ jtech-services-api-gateway/src/test/java/br/com/jtech/cloud/api/gateway/JtechServicesApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jtech.cloud.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtechServicesApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
