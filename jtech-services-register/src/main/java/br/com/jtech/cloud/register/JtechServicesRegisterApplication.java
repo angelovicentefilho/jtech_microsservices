d7ac9e91d98ba55d9e51ffbde6c4b6f79a408b92
Angelo Vicente Filho <angelo.vicente@jtech.com.br>
2021-07-05 14:18:40 -0300

    [main] Create Register

---
+++ jtech-services-register/src/main/java/br/com/jtech/cloud/register/JtechServicesRegisterApplication.java
@@ -0,0 +1,13 @@
+package br.com.jtech.cloud.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtechServicesRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtechServicesRegisterApplication.class, args);
+	}
+
+}
---
+++ jtech-services-register/src/test/java/br/com/jtech/cloud/register/JtechServicesRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jtech.cloud.register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtechServicesRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
