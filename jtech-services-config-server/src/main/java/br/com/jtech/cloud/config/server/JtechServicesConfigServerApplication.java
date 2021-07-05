68946193ede9e01f4d2946065c8db370525afa6d
Angelo Vicente Filho <angelo.vicente@jtech.com.br>
2021-07-05 14:17:24 -0300

    [main] Create Config Server

---
+++ jtech-services-config-server/src/main/java/br/com/jtech/cloud/config/server/JtechServicesConfigServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.jtech.cloud.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtechServicesConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtechServicesConfigServerApplication.class, args);
+	}
+
+}
---
+++ jtech-services-config-server/src/test/java/br/com/jtech/cloud/config/server/JtechServicesConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jtech.cloud.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtechServicesConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
