f9428b8931bb6c0e55df38272f48f3ac3a093e9e
Angelo Vicente Filho <angelo.vicente@jtech.com.br>
2021-07-05 14:19:39 -0300

    [main] Create Users service

---
+++ jtech-services-users/src/main/java/br/com/jtech/cloud/users/JtechServicesUsersApplication.java
@@ -0,0 +1,13 @@
+package br.com.jtech.cloud.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtechServicesUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtechServicesUsersApplication.class, args);
+	}
+
+}
---
+++ jtech-services-users/src/test/java/br/com/jtech/cloud/users/JtechServicesUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jtech.cloud.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtechServicesUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
