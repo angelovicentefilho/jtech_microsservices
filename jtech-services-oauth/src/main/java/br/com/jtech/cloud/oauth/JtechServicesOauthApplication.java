d9b303099ddbf931c77535c28756c4a2f1f53e33
Angelo Vicente Filho <angelo.vicente@jtech.com.br>
2021-07-05 14:20:43 -0300

    [main] Create OAuth Service

---
+++ jtech-services-oauth/src/main/java/br/com/jtech/cloud/oauth/JtechServicesOauthApplication.java
@@ -0,0 +1,13 @@
+package br.com.jtech.cloud.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtechServicesOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtechServicesOauthApplication.class, args);
+	}
+
+}
---
+++ jtech-services-oauth/src/test/java/br/com/jtech/cloud/oauth/JtechServicesOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jtech.cloud.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtechServicesOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
