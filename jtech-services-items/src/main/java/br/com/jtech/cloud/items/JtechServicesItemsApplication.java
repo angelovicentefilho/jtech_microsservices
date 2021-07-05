4fc8f021f1afa761da0a472c0659e6af9fcf3f9c
Angelo Vicente Filho <angelo.vicente@jtech.com.br>
2021-07-05 14:14:11 -0300

    [main] Create Items service

---
+++ jtech-services-items/src/main/java/br/com/jtech/cloud/items/JtechServicesItemsApplication.java
@@ -0,0 +1,13 @@
+package br.com.jtech.cloud.items;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtechServicesItemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtechServicesItemsApplication.class, args);
+	}
+
+}
---
+++ jtech-services-items/src/test/java/br/com/jtech/cloud/items/JtechServicesItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jtech.cloud.items;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtechServicesItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
