dac83523818dc35c8230575e3defc3b899e6e2f0
Angelo Vicente Filho <angelo.vicente@jtech.com.br>
2021-07-05 14:12:44 -0300

    [main] Create Product Service

---
+++ jtech-services-product/src/main/java/br/com/jtech/cloud/product/JtechServicesProductApplication.java
@@ -0,0 +1,13 @@
+package br.com.jtech.cloud.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtechServicesProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtechServicesProductApplication.class, args);
+	}
+
+}
---
+++ jtech-services-product/src/test/java/br/com/jtech/cloud/product/JtechServicesProductApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jtech.cloud.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtechServicesProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
